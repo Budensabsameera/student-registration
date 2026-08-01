ee6f3dd15fa2303bbecbb40073f606585d0cb6d3
sameera <budensabsameera@gmail.com>
2026-08-01 23:36:15 +0530

    Connect frontend with Render backend

--- backend/src/main/java/com/sameera/backend/controller/StudentController.java
+++ backend/src/main/java/com/sameera/backend/controller/StudentController.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/students")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "*")
 public class StudentController {
 
     @Autowired
