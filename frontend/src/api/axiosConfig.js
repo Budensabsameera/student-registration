99a21a9e66e7ba4d8d2e1ba849c8f55fba36c356
sameera <budensabsameera@gmail.com>
2026-08-02 07:40:42 +0530

    Fix MySQL AUTO_INCREMENT issue

--- frontend/src/api/axiosConfig.js
+++ frontend/src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://student-registration-backend-cl7y.onrender.com"
+  baseURL: "https://student-registration-6ba1.onrender.com"
 });
 
 export default API;
